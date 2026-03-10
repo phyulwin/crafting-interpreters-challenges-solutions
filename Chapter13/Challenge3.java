2378fded4f5f6397950fb0d4a56e1f583100a0db
phyulwin <kelly-phl@Mac.lan>
2026-03-10 00:57:49 -0700

    chapter13 challenges

--- Chapter13/Challenge3.java
+++ Chapter13/Challenge3.java
@@ -1,23 +1,44 @@
-// The following changes are made in the actual forked repository files
+Challenge3lowing changes are made in the actual forked repository files
 // and copied here for assignment submission. 
 
-class Doughnut {
-cook() {
-    print "Fry until golden brown.";
-    inner();
-    print "Place in a nice box.";
-}
+// Additional Feature Implemented: List Type
+
+// LoxList.java
+class LoxList {
+
+  private final List<Object> values;
+
+  LoxList(List<Object> values) {
+    this.values = values;
+  }
+
+  Object get(int index) {
+    return values.get(index);
+  }
+
+  void set(int index, Object value) {
+    values.set(index, value);
+  }
+
+  void add(Object value) {
+    values.add(value);
+  }
+
+  @Override
+  public String toString() {
+    return values.toString();
+  }
 }
 
-class BostonCream < Doughnut {
-cook() {
-    print "Pipe full of custard and coat with chocolate.";
-}
-}
+// Interpreter.java
+@Override
+public Object visitListExpr(Expr.List expr) {
 
-BostonCream().cook();
+  List<Object> values = new ArrayList<>();
 
-// Output
-// Fry until golden brown.
-// Pipe full of custard and coat with chocolate.
-// Place in a nice box.
+  for (Expr element : expr.elements) {
+    values.add(evaluate(element));
+  }
+
+  return new LoxList(values);
+}
